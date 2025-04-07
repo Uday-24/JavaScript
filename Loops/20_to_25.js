568fa944fd45ea2f00e7b5ee6855d19a30f0fe57
Uday-24 <chotaliyauday24@gmail.com>
2025-04-07 09:48:01 +0530

    New loops added

---
+++ Loops/20_to_25.js
@@ -0,0 +1,73 @@
+let i, j, pyramid;
+
+size = 5;
+for(i=size; i>=1; i--){
+    pyramid = "";
+    for(j=i; j<=size; j++){
+        pyramid += i + " ";
+    }
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+size = 5;
+k = 1;
+for(i=1; i<=size; i++){
+    pyramid = "";
+    for(j=1; j<=i; j++){
+        pyramid += k + " ";
+        k++;
+    }
+    k--;
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+size = 5;
+for(i=size; i>=1; i--){
+    pyramid = "";
+    for(j=1; j<=i; j++){
+        pyramid += j + " ";
+    }
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+size = 5;
+for(i=1; i<=size; i++){
+    pyramid = "";
+    for(j=size; j>=i; j--){
+        pyramid += j + " ";
+    }
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+size = 5;
+for(i=1; i<=size; i++){
+    pyramid = "";
+    for(j=i; j<=size; j++){
+        pyramid += j + " ";
+    }
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+size = 5;
+for(i=size; i>=1; i--){
+    pyramid = "";
+    for(j=i; j>=1; j--){
+        pyramid += j + " ";
+    }
+    console.log(pyramid);
+}
