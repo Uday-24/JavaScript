a3434656d7da871b16b59d143129fa7388d29897
Uday-24 <chotaliyauday24@gmail.com>
2025-04-19 08:28:43 +0530

    new pyramid added

---
+++ Loops/62_to_65.js
@@ -0,0 +1,68 @@
+let i, j, str, size, pyramid;
+
+str = "abcde";
+size = str.length;
+
+for(i=size; i>=1; i--){
+    pyramid = "";
+    for(j=i+1; j<=size; j++){
+        pyramid += "  ";
+    }
+    for(j=1; j<=i; j++){
+        pyramid += " " + str[j-1];
+    }
+    console.log(pyramid)
+}
+
+console.log();
+console.log();
+
+str = "ABCDE";
+size = str.length;
+
+for(i=size; i>=1; i--){
+    pyramid = "";
+    for(j=i+1; j<=size; j++){
+        pyramid += "  ";
+    }
+    for(j=1; j<=i; j++){
+        pyramid += " " + str[j-1];
+    }
+    console.log(pyramid)
+}
+
+
+console.log();
+console.log();
+
+str = "abcde";
+size = str.length;
+
+for(i=1; i<=size; i++){
+    pyramid = "";
+    for(j=1; j<i; j++){
+        pyramid += "  ";
+    }
+    for(j=i; j<=size; j++){
+        pyramid += " " + str[j-1];
+    }
+    console.log(pyramid);
+}
+
+
+console.log();
+console.log();
+
+str = "ABCDE";
+size = str.length;
+
+for(i=1; i<=size; i++){
+    pyramid = "";
+    for(j=1; j<=i; j++){
+        pyramid += "  ";
+    }
+    for(j=i; j<=size; j++){
+        pyramid += " " + str[j-1];
+    }
+    console.log(pyramid);
+}
