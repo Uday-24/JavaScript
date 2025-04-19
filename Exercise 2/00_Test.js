3ab25ce068314a1dc581759ef137c0f0ad3aedcd
Uday-24 <chotaliyauday24@gmail.com>
2025-04-19 09:01:31 +0530

    Sorting technique practise

--- Exercise 2/00_Test.js
+++ Exercise 2/00_Test.js
@@ -1,16 +1,29 @@
-const isPalindrome = (num) => {
-    let reversed = 0;
-    let temp = num;
-
-    while(temp > 0){
-        let rem = temp % 10;
-        reversed = (reversed*10) + rem;
-        temp /= 10;
-        temp = Math.floor(temp);
+function bubbleSort(arr){
+    let len = arr.length;
+    for(let i=1; i < len; i++){
+        for(let j=0; j < len-i; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
     }
-    if(reversed == num) return true;
-    else return false;
+    return arr;
 }
 
-let num = 131;
-console.log(isPalindrome(num));
+function selectionSort(arr){
+    let len = arr.length;
+    for(let i=0; i<=len-2; i++){
+        for(j=i+1; j<=len-1; j++){
+            if(arr[i] > arr[j]){
+                let temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort([4, 1, 5, 2, 3, 9, 8, 5, 4, 7]));
