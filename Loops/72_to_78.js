08e49452c945f02332449a1222abcd8d98856436
Uday-24 <chotaliyauday24@gmail.com>
2025-04-28 09:25:38 +0530

    Pyramids added

--- Loops/72_to_78.js
+++ Loops/72_to_78.js
@@ -32,3 +32,95 @@ for(i=1; i<=size; i++){
     }
     console.log(pyramid);
 }
+
+str = "abcde";
+size = str.length;
+for(i=1; i<=size; i++){
+    pyramid = "";
+    for(j=i+1; j<=size; j++){
+        pyramid += "  ";
+    }
+    for(j=1; j<i; j++){
+        pyramid += str[j-1] + " ";
+    }
+    for(j=i; j>=1; j--){
+        pyramid += str[j-1] + " ";
+    }
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+str = "ABCDE";
+size = str.length;
+for(i=1; i<=size; i++){
+    pyramid = "";
+    for(j=i+1; j<=size; j++){
+        pyramid += "  ";
+    }
+    for(j=1; j<i; j++){
+        pyramid += str[j-1] + " ";
+    }
+    for(j=i; j>=1; j--){
+        pyramid += str[j-1] + " ";
+    }
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+str = "abcde";
+size = str.length;
+
+for(i=size; i>=1; i--){
+    pyramid = "";
+    for(j=1; j<i; j++){
+        pyramid += "  ";
+    }
+    for(j=size; j>i; j--){
+        pyramid += str[j-1] + " ";
+    }
+    for(j=i; j<=size; j++){
+        pyramid += str[j-1] + " ";
+    }
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+str = "ABCDE";
+size = str.length;
+for(i=size; i>=1; i--){
+    pyramid = "";
+    for(j=1; j<i; j++){
+        pyramid += "  ";
+    }
+    for(j=i; j<=size-1; j++){
+        pyramid += str[j-1] + " ";
+    }
+    for(j=size; j>=i; j--){
+        pyramid += str[j-1] + " ";
+    }
+    console.log(pyramid);
+}
+
+console.log();
+console.log();
+
+size = 5;
+for(i=size; i>=1; i--){
+    pyramid = "";
+    for(j=1; j<i; j++){
+        pyramid += "  ";
+    }
+    for(j=size; j>i; j--){
+        pyramid += j + " ";
+    }
+    for(j=i; j<=size; j++){
+        pyramid += j + " ";
+    }
+    console.log(pyramid);
+}
